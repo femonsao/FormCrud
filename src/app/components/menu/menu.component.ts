4473bdd97e1403783d656fa900d7e5201a311723
Felipe monsão <femonsao@gmail.com>
2022-02-08 21:30:15 -0300

    wip  - componente de menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  public isCreate: boolean = false;
+  public isEdit: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public backPage(){
+    window.history.back();
   }
 
 }
